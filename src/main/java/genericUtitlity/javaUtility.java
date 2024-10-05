157dd974a1a0d719c8a81052e5b4537e5d7a9ac1
Akankshakaurav111 <jkaurav29@gmail.com>
2024-10-05 10:03:57 +0530

    1st commit in git hub via gitBush

---
+++ src/main/java/genericUtitlity/javaUtility.java
@@ -0,0 +1,5 @@
+package genericUtitlity;
+
+public class javaUtility {
+
+}
---
+++ src/test/java/createGitHub/GitRemotTest.java
@@ -0,0 +1,10 @@
+package createGitHub;
+
+public class GitRemotTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
